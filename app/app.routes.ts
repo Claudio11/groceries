22f9c6e6f1de9de6c62e0d611ccf48049936b5d0
Claudio Soto <csoto@altimetrik.com>
2016-10-09 14:37:18 -0400

    Cleaned routes commented code.

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -5,18 +5,6 @@ import { ShoppingListRoutingModule } from './components/container/shoppingList/s
 
 import { ShoppingListDetailComponent } from './components/container/shoppingList/shoppingListDetail.component';
 
-// Route Configuration
-// export const routes: Routes = [
-//   {
-//     path: '',
-//     redirectTo: '/shoppingList',
-//     pathMatch: 'full'
-//   },
-//   { path: 'shoppingList', component: ShoppingListDetailComponent }, // Put list of shoppingList here later
-// ];
-//
-// export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
-
 @NgModule({
   imports: [
       RouterModule.forRoot([
